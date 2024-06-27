770709309cd980ba25ab439552f9d8801416198c
Axel-Leger <axelleger2@gmail.com>
2024-06-27 14:52:51 -0300

    se creo las rutas para obtener todos los libros, por id y crear

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 const express = require("express")
+const dbLibros = require("./db")
 
 const app = express()
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log("el servidor se esta corriendo en el puerto: ",PORT) )
+
+//RUTAS
+
+//OBTENER TODOS LOS LIBROS
+app.get("/books", (req,res)=>{
+    res.json(dbLibros)
+})
+
+//OBTENER LIBROS POR ID
+app.get("/books/:id", (req,res)=>{
+    const id = parseInt(req.params.id)
+
+    const encontrarID = dbLibros.find((libro)=> libro.id === id)
+
+    res.json(encontrarID)
+})
+
+//CREAR LIBRO
+
+app.post("/books", (req,res)=>{
+    const {id, title, author, year } = req.body
+
+    const NuevoLibro = dbLibros.push({id, title, author, year })
+
+    res.json({mensaje:"se creo con exito el nuevo libro"})
+})
---
+++ src/db.js
@@ -0,0 +1,22 @@
+const dbLibros = [
+    {
+        "id": 1,
+        "title": "PRIEMER LIBRO",
+        "author": "ALAN",
+        "year": 36
+    },
+    {
+        "id": 2,
+        "title": "SEGUNDO LIBRO",
+        "author": "MARIA",
+        "year": 54
+    },
+    {
+        "id": 3,
+        "title": "TERCER LIBRO",
+        "author": "JOSE",
+        "year": 40
+    }
+]
+
+module.exports = dbLibros
