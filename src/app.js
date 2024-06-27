175d93170b45e5f7a5e7599274d0831b90927067
Axel-Leger <axelleger2@gmail.com>
2024-06-27 15:12:38 -0300

    se creo las rutas para poder eliminar y modificar los libros

--- src/app.js
+++ src/app.js
@@ -19,17 +19,53 @@ app.get("/books", (req,res)=>{
 app.get("/books/:id", (req,res)=>{
     const id = parseInt(req.params.id)
 
-    const encontrarID = dbLibros.find((libro)=> libro.id === id)
+    
 
-    res.json(encontrarID)
+    try {
+        const encontrarID = dbLibros.find((libro)=> libro.id === id)
+    
+        if( !encontrarID){
+            res.json({mensaje:"el id que coloco no existe"})
+        }else{
+            res.json(encontrarID)
+        }
+    } catch (error) {
+        res.json({mensaje:"eror al encontrat el id"})
+    }
 })
 
 //CREAR LIBRO
-
 app.post("/books", (req,res)=>{
     const {id, title, author, year } = req.body
 
     const NuevoLibro = dbLibros.push({id, title, author, year })
 
     res.json({mensaje:"se creo con exito el nuevo libro"})
+})
+
+//MODIFICAR LIBRO
+
+app.put("/books/:id", (req,res)=>{
+    const id = parseInt(req.params.id)
+    const {title,author,year} = req.body
+
+    const encontrarID = dbLibros.find((libro) => libro.id === id)
+
+    encontrarID.title = title
+    encontrarID.author = author
+    encontrarID.year = year
+
+    res.json({mensaje: "se modifico con exito el titulo"})
+})
+
+//ELIMINAR LIBRO
+
+app.delete("/books/:id", (req,res)=>{
+    const id = parseInt(req.params.id)
+
+    const encontrarID = dbLibros.find((libro)=>libro.id === id)
+    const almacenarID = dbLibros.indexOf(encontrarID)
+    const eliminarID = dbLibros.splice(almacenarID, 1)
+
+    res.json({mensaje: "se elimino el libro correcatemente"})
 })
