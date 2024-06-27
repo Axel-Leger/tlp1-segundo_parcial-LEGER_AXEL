56e7a1ddaeba3a6f8ccea4bfb2ad26b41260ca30
Axel-Leger <axelleger2@gmail.com>
2024-06-27 16:38:03 -0300

    se termino el examen

--- src/app.js
+++ src/app.js
@@ -19,16 +19,14 @@ app.get("/books", (req,res)=>{
 app.get("/books/:id", (req,res)=>{
     const id = parseInt(req.params.id)
 
-    
-
     try {
         const encontrarID = dbLibros.find((libro)=> libro.id === id)
-    
         if( !encontrarID){
-            res.json({mensaje:"el id que coloco no existe"})
+            res.json({mensaje:"el id del libro que coloco no existe"})
         }else{
             res.json(encontrarID)
         }
+
     } catch (error) {
         res.json({mensaje:"eror al encontrat el id"})
     }
@@ -38,34 +36,57 @@ app.get("/books/:id", (req,res)=>{
 app.post("/books", (req,res)=>{
     const {id, title, author, year } = req.body
 
-    const NuevoLibro = dbLibros.push({id, title, author, year })
-
-    res.json({mensaje:"se creo con exito el nuevo libro"})
+    try {
+        const encontrarID = dbLibros.find((libro)=>libro.id === id)
+        if(!encontrarID){
+            const NuevoLibro = dbLibros.push({id, title, author, year })
+            res.json({mensaje:"se creo con exito el nuevo libro"})
+        }else{
+            res.json({mensaje:"ya hay otro libro con el mismo id"})
+        }
+    } catch (error) {
+        res.json({mensaje:"error al buscar y crear libro"})
+    }
 })
 
-//MODIFICAR LIBRO
 
+//MODIFICAR LIBRO
 app.put("/books/:id", (req,res)=>{
     const id = parseInt(req.params.id)
     const {title,author,year} = req.body
 
-    const encontrarID = dbLibros.find((libro) => libro.id === id)
-
-    encontrarID.title = title
-    encontrarID.author = author
-    encontrarID.year = year
-
-    res.json({mensaje: "se modifico con exito el titulo"})
+    try {
+        const encontrarID = dbLibros.find((libro) => libro.id === id)
+        if (!encontrarID){
+            res.json({mensaje:"no se pudo en contrar el id para modificar"})
+        }else{
+            encontrarID.title = title
+            encontrarID.author = author
+            encontrarID.year = year
+        
+            res.json({mensaje: "se modifico con exito el titulo"})
+        }
+    } catch (error) {
+        res.json({mensaje:"error a en contrar libro para modificar"})
+    }
 })
 
-//ELIMINAR LIBRO
 
+//ELIMINAR LIBRO
 app.delete("/books/:id", (req,res)=>{
     const id = parseInt(req.params.id)
 
-    const encontrarID = dbLibros.find((libro)=>libro.id === id)
-    const almacenarID = dbLibros.indexOf(encontrarID)
-    const eliminarID = dbLibros.splice(almacenarID, 1)
+    try {
+        const encontrarID = dbLibros.find((libro)=>libro.id === id)
+        if (!encontrarID){
+            res.json({mensaje:"no se pudo encontrar el id del libro que desea eliminar"})
+        }else{
+            const almacenarID = dbLibros.indexOf(encontrarID)
+            const eliminarID = dbLibros.splice(almacenarID, 1)
 
-    res.json({mensaje: "se elimino el libro correcatemente"})
+            res.json({mensaje: "se elimino el libro correcatemente"})
+        }
+    } catch (error) {
+        res.json({mensaje:"error al buscar el id del libro que desea eliminar"})
+    }
 })
